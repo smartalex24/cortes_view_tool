0f8a88e4f793ccb49659136f5345ffc315f8a81d
Alex Cortes <alexcortes247@gmail.com>
2017-04-24 13:41:46 -0600

    Cortes tool initial commit

---
+++ lib/cortes_view_tool.rb
@@ -0,0 +1,6 @@
+require "cortes_view_tool/version"
+require "cortes_view_tool/renderer"
+
+module CortesViewTool
+  # Your code goes here...
+end
---
+++ lib/cortes_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CortesViewTool
+  VERSION = "0.1.0"
+end
